41012fc6db8d50cb939145ea64260b0245a421c7
Alli Tzadok <allisontzadok@gmail.com>
2018-01-13 23:55:56 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages].to_a_string
+		 programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
