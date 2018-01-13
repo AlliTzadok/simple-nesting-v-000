eb86db9b1825f8e488063cd1c310c3a11fd1573d
Alli Tzadok <allisontzadok@gmail.com>
2018-01-13 23:54:04 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -39,8 +39,6 @@ def alan_kay_is_known_for
      }
 
 		 programmer_hash[:alan_kay][:known_for]
-
-
 end
 
 def dennis_ritchies_language
@@ -59,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
