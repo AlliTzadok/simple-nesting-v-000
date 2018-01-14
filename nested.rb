da53455c941c7a7db53fa0b78e63c70a48cc3942
Alli Tzadok <allisontzadok@gmail.com>
2018-01-14 00:02:15 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -60,13 +60,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
-programmer_hash << {:yukihiro_matsumoto
 
 	programmer_hash =
  		{
@@ -83,6 +76,8 @@ programmer_hash << {:yukihiro_matsumoto
           :languages => ["C"]
         }
      }
+
+
 
 
 end
